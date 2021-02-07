827d2a8023e62f9e6483cf1b23ef11ec4b11deab
lishanyi-66 <you@example.com>
2021-02-07 17:38:11 +0800

    aaa

--- src/components/userDetail.vue
+++ src/components/userDetail.vue
@@ -1,9 +1,9 @@
 <template>
-  <div>
-    <div class="backgoundImg"></div>
+  <div class="m-space-info">
+    <div class="banner"></div>
     <div class="part1">
       <div class="face">
-        头像
+        <div class="imgface"></div>
       </div>
       <div class="relation">
         <div class="count">
@@ -22,7 +22,21 @@
         </div>
         <!-- 编辑资料按钮 -->
         <div class="follow-btn">
-          <button>编辑资料</button>
+          <div>编辑资料</div>
+        </div>
+      </div>
+    </div>
+    <div class="part2">
+      <div class="base">
+        <div class="name">昵称</div>
+        <div class="level">等级</div>
+      </div>
+      <div class="desc">
+        <div class="content">
+          这个人很神秘，什么都没留下
+        </div>
+        <div class="spread-btn">
+          <a href="#">展开</a>
         </div>
       </div>
     </div>
@@ -34,26 +48,42 @@ export default {};
 </script>
 
 <style lang='less' scope>
-.backgoundImg {
+.m-space-info{
+width: 100%;
+background-color: white;
+.banner {
   //   background-image: "../assets/bannerTop_new.png";
   width: 100%;
   height: 90px;
   //   background-color: red;
   background-image: url("../assets/bannerTop_new.png");
   background-repeat: no-repeat;
-  //   background-position: center;
+    background-position: center;
   background-size: 100%;
 }
 .part1 {
-  background-color: white;
-  width: 100%;
-  height: 300px;
+  // background-color: white;
+  
   display: flex;
+  // margin-right: 10px;
   .face{
       flex: 2;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      .imgface{
+        width: 80px;
+        height: 80px;
+        background-color: royalblue;
+        border-radius: 50px;
+      }
+      // background-color: red;
     }
   .relation{
     flex: 4;
+    margin: 10px 10px 10px 0;
+    color: #999;
+    font-size: 14px;
     .count{
       
       display: flex;
@@ -62,15 +92,77 @@ export default {};
       .fans{
         flex: 1;
         text-align: center;
+        position: relative;
+        &::after{
+          content:'';
+          border-right: 1px solid #999;
+          position:absolute;
+          // color:red;
+          bottom: 0;
+          right: 0;
+          top: 0;
+          width: 1px;
+          height: 40px;
+          transform: scale(0.5);
+          transform-origin: left;
+        }
       }
       .focus{
         flex: 1;
+        text-align: center;
+        // border-right: 1px solid red;
+        position:relative;
+        &::after{
+          content:'';
+          border-right: 1px solid #999;
+          position:absolute;
+          // color:red;
+          bottom: 0;
+          right: 0;
+          top: 0;
+          width: 1px;
+          height: 40px;
+          transform: scale(0.5);
+          transform-origin: left;
+        }
       }
       .likes{
         flex: 1;
+        text-align: center;
       }
       
     }
   }
+  .follow-btn{
+    width: 100%;
+    border: 1px solid #fb7299;
+    padding: 5px;
+    border-radius: 5px;
+    margin: 10px 0;
+    text-align: center;
+    color:#fb7299;
+  }
 }
+.part2{
+  padding: 10px;
+  .base{
+    display: flex;
+    // justify-content: center;
+    align-items: center;
+    
+    .name{
+      font-size: 22px;
+      margin-right: 10px;
+    }
+
+  }
+  .desc{
+    display: flex;
+    justify-content: space-between;
+    font-size: 14px;
+    margin-top: 10px;
+  }
+}
+}
+
 </style>
