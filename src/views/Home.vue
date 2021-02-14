53eac0914d96fb2a342aa38f94b6f7baeda82ccd
lishanyi-66 <lishanyi18@gmail.com>
2021-02-15 00:11:27 +0800

    fff

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -26,8 +26,44 @@ export default {
   methods: {
     async SelectCategory() {
       const res = await this.$http.get("/category");
+      //   console.log(res);
+      this.changeCategry(res.data);
+    },
+    // 处理数据
+    changeCategry(data) {
+      const category1 = data.map((item, index) => {
+        item.list = [];
+        item.page = 0;
+        item.pagesize = 10;
+        return item;
+      });
+      //   console.log(category1);
+      this.category = category1;
+      //   console.log(this.category);
+    },
+    // 获取首页内容的方法
+    async selectArticle() {
+      const numId = this.categoryItem();
+      //   console.log(numId);
+      const res = await this.$http.get("/detail/", numId._id, {
+        params: {
+          page: numId.page,
+          pagesize: numId.pagesize,
+        },
+      });
       console.log(res);
-      this.category = res.data;
+    },
+    // 获取文件内容下标的方法
+    categoryItem() {
+      const categoryItem = this.category[this.active];
+      //   console.log(categoryItem);
+      return categoryItem;
+    },
+  },
+  watch: {
+    active() {
+      //   this.categoryItem();
+      this.selectArticle();
     },
   },
   created() {
