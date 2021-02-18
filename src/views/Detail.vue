5a113915b82a28e898db523f15e132fe05351138
lishanyi-66 <lishanyi18@gmail.com>
2021-02-18 08:04:11 +0800

    ttt

---
+++ src/views/Detail.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="box">
+    <!-- <div>{{ detailitem.id }}</div> -->
+    <img src="" alt="" />
+    <div>{{ detailitem.name }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["detailitem"],
+};
+</script>
+
+<style  scope>
+.box img {
+  width: 100%;
+  height: 140px;
+  background-color: #eee;
+  margin-bottom: 10px;
+
+  /* display: block; */
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,7 +7,14 @@
         :title="item.title"
         :key="index"
       >
-        内容 {{ index }}
+        <div class="detailparent">
+          <detail
+            class="detailitem"
+            :detailitem="categoryitem"
+            v-for="(categoryitem, categoryindex) in item.list"
+            :key="categoryindex"
+          ></detail>
+        </div>
       </van-tab>
     </van-tabs>
   </div>
@@ -15,6 +22,7 @@
 
 <script>
 import NavBar from "../components/NavBar.vue";
+import Detail from "./Detail.vue";
 export default {
   data() {
     return {
@@ -22,7 +30,7 @@ export default {
       active: 0,
     };
   },
-  components: { NavBar },
+  components: { NavBar, Detail },
   methods: {
     async SelectCategory() {
       const res = await this.$http.get("/category");
@@ -40,18 +48,20 @@ export default {
       //   console.log(category1);
       this.category = category1;
       //   console.log(this.category);
+      //   处理数据时调用获取首页内容
+      this.selectArticle();
     },
     // 获取首页内容的方法
     async selectArticle() {
       const numId = this.categoryItem();
       //   console.log(numId);
-      const res = await this.$http.get("/detail/", numId._id, {
+      const res = await this.$http.get("/detail/" + numId._id, {
         params: {
           page: numId.page,
           pagesize: numId.pagesize,
         },
       });
-      console.log(res);
+      numId.list = res.data;
     },
     // 获取文件内容下标的方法
     categoryItem() {
@@ -72,5 +82,19 @@ export default {
 };
 </script>
 
-<style>
+<style lang='less' scope>
+.detailparent {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  padding: 5px;
+  //   padding: 10px;
+  .detailitem {
+    width: 48%;
+    margin-top: 20px;
+    font-size: 14px;
+    // height: 120px;
+    // background-color: tomato;
+  }
+}
 </style>
