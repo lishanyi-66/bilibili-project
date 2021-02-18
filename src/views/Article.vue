972395e7eedf2f2e78084808bfaf99463be0717f
lishanyi-66 <lishanyi18@gmail.com>
2021-02-19 00:12:08 +0800

    ooo

--- src/views/Article.vue
+++ src/views/Article.vue
@@ -1,17 +1,51 @@
 <template>
   <div>
-      <nav-bar></nav-bar>
+    <nav-bar></nav-bar>
+    <div class="videobox">
+      <div class="video">
+        <video :src="model.content" controls="controls"></video>
+      </div>
+    </div>
+    <div class="detailinfo">
+      <div>{{ model.name }}</div>
+    </div>
   </div>
 </template>
 
 <script>
-import NavBar from '../components/NavBar.vue'
+import NavBar from "../components/NavBar.vue";
 export default {
   components: { NavBar },
-
-}
+  data() {
+    return {
+      model: {},
+    };
+  },
+  methods: {
+    async articleitemData() {
+      const res = await this.$http.get("/article/" + this.$route.params.id);
+      console.log(res);
+      this.model = res.data[0];
+    },
+  },
+  created() {
+    this.articleitemData();
+  },
+};
 </script>
 
-<style>
-
+<style lang='less' scope>
+.videobox {
+  // width: 100%;
+  .video {
+    width: 100%;
+    video {
+      width: 100%;
+    }
+  }
+}
+.detailinfo {
+  padding: 15px;
+  font-size: 18px;
+}
 </style>
--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -1,22 +1,21 @@
 <template>
-  <div class="box" @click="$router.push('/detail'+detailitem.id+'')">
-    
+  <div class="box" @click="$router.push(`/article/${detailitem.id}`)">
     <div class="imgparent">
-         <img src="" alt="" />
-    <div class="outer">
-      <div class="evaluationBar">
-        <span>
-          <van-icon name="play-circle-o" size="18" />
-          <i>699</i>
+      <img src="" alt="" />
+      <div class="outer">
+        <div class="evaluationBar">
+          <span>
+            <van-icon name="play-circle-o" size="18" />
+            <i>699</i>
           </span>
-        <span>
-          <van-icon name="chat-o" size="18" />
-          <i>66</i>
-        </span>
+          <span>
+            <van-icon name="chat-o" size="18" />
+            <i>66</i>
+          </span>
+        </div>
       </div>
     </div>
-    </div>
-   
+
     <div>{{ detailitem.name }}</div>
   </div>
 </template>
@@ -28,44 +27,39 @@ export default {
 </script>
 
 <style  scope lang='less'>
-.box{
-  position:relative;
+.box {
+  position: relative;
 }
 .box img {
   width: 100%;
   height: 120px;
   background-color: #eee;
   margin-bottom: 8px;
-  
-
 }
-.imgparent{
-  position:relative;
+.imgparent {
+  position: relative;
 }
-.outer{
+.outer {
   width: 100%;
   position: absolute;
   left: 0;
   bottom: 0;
   // top: 88px;
-  
-  
 }
-.evaluationBar{
-    display: flex;
-    justify-content:space-between;
-    padding: 5px;
-    margin-bottom: 10px;
-    // position: absolute;
-    //  vertical-align: middle; 
-    span{
-      font-size: 14px;
-      i{
-        // display: block;
-        margin-left: 4px;
-        vertical-align:middle;
-      }
+.evaluationBar {
+  display: flex;
+  justify-content: space-between;
+  padding: 5px;
+  margin-bottom: 10px;
+  // position: absolute;
+  //  vertical-align: middle;
+  span {
+    font-size: 14px;
+    i {
+      // display: block;
+      margin-left: 4px;
+      vertical-align: middle;
     }
-    
   }
+}
 </style>
