7519df5bc6b59eb8f0d397855e46160111d05f7c
lishanyi-66 <lishanyi18@gmail.com>
2021-02-21 00:57:24 +0800

    ppp

--- src/views/Article.vue
+++ src/views/Article.vue
@@ -6,8 +6,19 @@
         <video :src="model.content" controls="controls"></video>
       </div>
     </div>
+    <!-- 打开app 按钮 -->
+    <div>
+      <div class="btnapp">
+        <span>打开app,流畅又高清</span>
+      </div>
+    </div>
     <div class="detailinfo">
-      <div>{{ model.name }}</div>
+      <div class="hotbtn">
+        <span><van-icon name="fire" />热门</span>
+        <div>{{ model.name }}</div>
+      </div>
+
+      <van-icon name="arrow-down" />
     </div>
   </div>
 </template>
@@ -47,5 +58,33 @@ export default {
 .detailinfo {
   padding: 15px;
   font-size: 18px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.btnapp {
+  width: 90%;
+  background-color: #fb7299;
+  padding: 10px;
+  // scale: 50%;
+  border-radius: 30px;
+  text-align: center;
+  margin: auto;
+  margin-top: 10px;
+  span {
+    color: white;
+  }
+}
+.hotbtn {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  span {
+    padding: 6px;
+    background-color: #eee;
+    border-radius: 30px;
+    font-size: 15px;
+    color: #fb7299;
+  }
 }
 </style>
